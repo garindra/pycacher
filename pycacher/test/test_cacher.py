e223e8605b79b72d1605010bee3c244fea054a73
Garindra Prahandono <garindraprahandono@gmail.com>
2012-08-06 11:56:43 -0700

    added cacher initialization test

--- pycacher/test/test_cacher.py
+++ pycacher/test/test_cacher.py
@@ -1,4 +1,24 @@
 import unittest
 
+from pycacher import Cacher
+from pycacher.backends import MemcacheBackend, LocalBackend
+
 class CacherTestCase(unittest.TestCase):
-    pass
+    
+    def test_init_without_args(self):
+        
+        cacher = Cacher()
+
+        self.assertTrue(isinstance(cacher.backend, MemcacheBackend))
+
+    def test_init_custom_backend(self):
+        
+        cacher = Cacher(backend=LocalBackend())
+
+        self.assertTrue(isinstance(cacher.backend, LocalBackend))
+
+    def test_init_custom_host_port(self):
+        
+        cacher = Cacher('localhost', 11211)
+
+        self.assertTrue(isinstance(cacher.backend, MemcacheBackend))
