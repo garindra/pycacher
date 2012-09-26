a4954c5c07e2a2692b9d5991d4f599684e0d97af
Garindra Prahandono <garindraprahandono@gmail.com>
2012-09-26 15:16:20 +0700

    moved decorators to its own module and set up initial placeholder of cache_list functionality

--- pycacher/decorators.py
+++ pycacher/decorators.py
@@ -1,6 +1,121 @@
+import pickle
 
-class CachedFunction(object):
-    pass
+from .utils import default_cache_key_func
+from .exceptions import InvalidHookEventException, OutOfBatcherContextRegistrationException
 
-def cached():
-    pass
+class CachedFunctionDecorator(object):
+    
+    def __init__(self, func, cacher=None, expires=None, 
+                        cache_key_func=default_cache_key_func):
+        self.func = func
+        self.cacher = cacher
+        self.cache_key_func = cache_key_func
+        self.expires = expires
+
+    def __call__(self, *args, **kwargs):
+        """The method that will actually be called when the decorated functon
+        is called."""
+
+        cache_key = self._build_cache_key(*args)
+        
+        batcher = self.cacher.get_current_batcher()
+
+        if batcher:
+            unpickled_value = batcher.get(cache_key) or self.cacher.backend.get(cache_key)
+        else:
+            unpickled_value = self.cacher.backend.get(cache_key)
+
+        if unpickled_value is not None:
+            return pickle.loads(unpickled_value)
+        else:
+            value = self.func(*args)
+            self.cacher.backend.set(cache_key, pickle.dumps(value))
+            return value
+
+    def _build_cache_key(self, *args):
+        """Builds the cache key with the supplied cache_key function """
+        return self.cache_key_func(self.func, *args)
+
+    def build_cache_key(self, *args):
+        """Builds the cache key with the supplied cache_key function """
+        return self.cache_key_func(self.func, *args)
+
+    def warm(self, *args):
+        """
+
+            Forces to run the actual function (regardless of whether we already
+            have the result on the cache or not) and set the backend to store
+            the return value.
+
+        """
+        cache_key = self._build_cache_key(*args)
+
+        value = self.func(*args)
+        return self.cacher.backend.set(cache_key, pickle.dumps(value))
+
+    def is_cached(self, *args):
+        """
+            Simply checks if the current function value with the supplied args
+            is currently cached in the backend.
+        """
+        cache_key = self._build_cache_key(*args)
+
+        return self.cacher.backend.exists(cache_key)
+
+    def invalidate(self, *args):
+        """Invalidates the current function's cache key with the current args.
+
+        Example usage::
+
+            is_user_board_subscriber.invalidate(uid, bid) 
+
+        """
+
+        key = self._build_cache_key(*args)
+
+        rv = self.cacher.backend.delete(key)
+        
+        #run all the invalidate hooks
+        for fn in self.cacher._hooks['invalidate']:
+            fn(key)
+
+        return rv
+
+    def register(self, *args):
+        """Registers the cached function on an active batcher context for later batching.
+            
+            Example usage::
+
+                batcher = cacher.create_batcher()
+
+                with batcher:
+                    cached_function.register(1, 2)
+
+                batcher.batch()
+
+                with batcher:
+                    cached_function(1, 2) #now will look for its value in the current batcher's
+                                          #last batched values.
+        """
+        batcher = self.cacher.get_current_batcher()
+
+        if batcher:
+            #Register the function and the args to the batcher 
+            batcher.register(self, *args)
+        else:
+            raise OutOfBatcherContextRegistrationException()
+
+class CachedListFunctionDecorator(CachedFunctionDecorator):
+    
+    def __init__(self, func, cacher=None, expires=None, 
+                        cache_key_func=default_cache_key_func):
+        self.func = func
+        self.cacher = cacher
+        self.cache_key_func = cache_key_func
+        self.expires = expires
+        self.range = range
+        self.skip_key = skip_key
+        self.limit_key = limit_key
+
+    def __call__(self, *args, **kwargs):
+        pass
