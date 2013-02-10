824a41a35653ed3631924644f70ac261115a67c6
Garindra Prahandono <garindraprahandono@gmail.com>
2013-02-10 13:49:37 -0800

    cleaned up backend tests

--- pycacher/test/test_backends.py
+++ pycacher/test/test_backends.py
@@ -9,10 +9,22 @@ from pycacher.backends import LocalBackend, MemcacheBackend
 #create the client
 client = memcache.Client(['localhost:11211'])
 
-class LocalBackendTestCase(unittest.TestCase):
-    
-    def setUp(self):
-        self.backend = LocalBackend()
+class BaseBackendTestCaseMixin(object):
+
+    def test_multi_get(self):
+        
+        self.backend.set('testkey1', 'testvalue1')
+        self.backend.set('testkey2', 'testvalue2')
+        self.backend.set('testkey3', 'testvalue3')
+
+        expected = {
+            
+            "testkey1" : "testvalue1",
+            "testkey2" : "testvalue2",
+            "testkey3" : "testvalue3"
+        }
+
+        assert self.backend.multi_get(['testkey1', 'testkey2', 'testkey3']) == expected
 
     def test_get_set(self):
         
@@ -20,21 +32,19 @@ class LocalBackendTestCase(unittest.TestCase):
 
         assert self.backend.get('testkey') == 'testvalue'
 
-    def test_multiget(self):
-        self.backend.set('testkey1', 'testvalue1')
-        self.backend.set('testkey2', 'testvalue2')
-        self.backend.set('testkey3', 'testvalue3')
+    def test_delete(self):
+        
+        self.backend.set('testkey1', 'test2')
+        self.backend.delete('testkey1')
 
-        expected = {
+        assert self.backend.get('testkey1') == None
 
-            "testkey1" : "testvalue1",
-            "testkey2" : "testvalue2",
-            "testkey3" : "testvalue3",
-        }
+class LocalBackendTestCase(unittest.TestCase, BaseBackendTestCaseMixin):
+    
+    def setUp(self):
+        self.backend = LocalBackend()
 
-        assert self.backend.multi_get(['testkey1', 'testkey2', 'testkey3']) == expected
-
-class MemcacheBackendTestCase(unittest.TestCase):
+class MemcacheBackendTestCase(unittest.TestCase, BaseBackendTestCaseMixin):
     
     def setUp(self):
         self.backend = MemcacheBackend(client)
@@ -44,30 +54,3 @@ class MemcacheBackendTestCase(unittest.TestCase):
         mock.__name__ = str(random.random() * 10)
 
         return mock
-
-    def test_get(self):
-        
-        client.set('testkey', 'testvalue')
-
-        assert client.get('testkey') == self.backend.get('testkey')
-
-    def test_multi_get(self):
-        
-        client.set('testkey1', 'testvalue1')
-        client.set('testkey2', 'testvalue3')
-        client.set('testkey3', 'testvalue3')
-
-        assert client.get_multi('testkey') == self.backend.multi_get('testkey')
-
-    def test_set(self):
-
-        self.backend.set('testsetkey', 'testsetvalue')
-
-        assert client.get('testsetkey') == 'testsetvalue'
-
-    def test_delete(self):
-        
-        client.set('testkey1', 'test2')
-        self.backend.delete('testkey1')
-
-        assert client.get('testkey1') == None
